2c2489c21e4846fea3ec7eb782292d86888151d3
JaredWeav <jared.weaver2@gmail.com>
2021-03-04 12:37:07 -0500

    adjusted h-bars

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -12,29 +12,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
         button.addEventListener('click', (e) => {
           // Grabs the id of the element that goes by the name, "id"
           const id = e.target.getAttribute('data-id');
+          console.log(e);
+          console.log(id)
 
           const burgerDevoured = {
             devoured: true
           }
   
-          fetch(`/api/burgers/${id}`, {
-            method: 'PUT',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
+        //   fetch(`/api/burgers/${id}`, {
+        //     method: 'PUT',
+        //     headers: {
+        //       Accept: 'application/json',
+        //       'Content-Type': 'application/json',
+        //     },
 
-            body: JSON.stringify(burgerDevoured),
+        //     body: JSON.stringify(burgerDevoured),
 
-          }).then((response) => {
-            console.log(response);
-            if (response.ok) {
-              console.log(`changed  to: ${id}`);
-              location.reload('/');
-            } else {
-              alert('something went wrong!');
-            }
-          });
+        //   }).then((response) => {
+        //     console.log(response);
+        //     if (response.ok) {
+        //       console.log(`changed  to: ${id}`);
+        //       location.reload('/');
+        //     } else {
+        //       alert('something went wrong!');
+        //     }
+        //   });
         });
       });
     }
